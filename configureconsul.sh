914ba7f73aeb26e6b42e0ec621bb3038012b2d1c
Nathan Dataguake Basanese <v6@users.noreply.github.com>
2018-06-18 17:56:37 -0700

    // , Upgrade Raft Protocol
      // , This will allow use of [Consul Autopilot features](https://www.consul.io/docs/agent/options.html#autopilot).

--- configureconsul.sh
+++ configureconsul.sh
@@ -2,6 +2,7 @@ bash -c "cat >/etc/consul.d/consul.json" << EOF
 {
   "server": true,
   "bootstrap_expect": 3,
+  "raft_protocol": 3,
   "datacenter": "superduper",
   "retry_join": ["192.168.13.35","192.168.13.36","192.168.13.37"],
   "advertise_addr": "$(hostname -I |grep -Eo '192\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')",
