edd9270d9ef0a910e9c114502a18641ebbc3cb73
v6 <github.com@basanese.com>
2018-06-28 14:08:03 -0700

    // , Make consul-replicate show correct version

--- consul-replicatedownload.sh
+++ consul-replicatedownload.sh
@@ -9,10 +9,10 @@ echo '  ##  https://github.com/hashicorp/consul-replicate#installation'
 curl -s -o "/tmp/consul-replicate_${VERSION}_linux_amd64.zip" "https://releases.hashicorp.com/consul-replicate/${VERSION}/consul-replicate_${VERSION}_linux_amd64.zip"
 
 unzip -o "/tmp/consul-replicate_${VERSION}_linux_amd64.zip" -d "${INSTALLDIR}"
-echo 'consul-replicate --version  ##  Result should contain v0.4.0'
-consul-replicate --version
 mkdir -v -m 755 '/etc/consul-replicate.d'
 chown -R consul-replicate:consul-replicate '/etc/consul-replicate.d'
 touch '/etc/consul-replicate.d/consul-replicate.hcl'
 chmod -R 0644 /etc/consul-replicate.d/*
 chown consul-replicate:consul-replicate '/usr/local/bin/consul-replicate'
+echo 'consul-replicate --version  ##  Result should contain v0.4.0'
+consul-replicate --version
