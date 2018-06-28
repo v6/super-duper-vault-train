334b0f6f366dffd9679cc5cf82f8cad73e4907c4
Nathan Basanese (git.io/v6) <v6@users.noreply.github.com>
2018-06-28 01:23:49 -0400

    // , Download consul-replicate
    This adds a script to download and extract the consul-replicate
    binary, because I want the option to set up
    consul-replicate.
    
    This also creates a consul-replicate user.
    
    It's installed on all 3 Vault/Consul instances, although only
    one instance really needs to have consul-replicate installed.

---
+++ consul-replicatedownload.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+VERSION=$1
+INSTALLDIR='/usr/local/bin/'
+curl --version
+
+echo "  ##  Consul Replicate Release: ${VERSION}"
+echo "  ##  Consul Replicate Download: https://releases.hashicorp.com/consul-replicate/${VERSION}/consul-replicate_${VERSION}_linux_amd64.zip"
+echo '  ##  https://github.com/hashicorp/consul-replicate#installation'
+curl -s -o "/tmp/consul-replicate_${VERSION}_linux_amd64.zip" "https://releases.hashicorp.com/consul-replicate/${VERSION}/consul-replicate_${VERSION}_linux_amd64.zip"
+
+unzip -o "/tmp/consul-replicate_${VERSION}_linux_amd64.zip" -d "${INSTALLDIR}"
+echo 'consul-replicate --version  ##  Result should contain v0.4.0'
+consul-replicate --version
+mkdir -v -m 755 '/etc/consul-replicate.d'
+chown -R consul-replicate:consul-replicate '/etc/consul-replicate.d'
+touch '/etc/consul-replicate.d/consul-replicate.hcl'
+chmod -R 0644 /etc/consul-replicate.d/*
+chown consul-replicate:consul-replicate '/usr/local/bin/consul-replicate'
