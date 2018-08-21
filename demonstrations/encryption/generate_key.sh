24a3586ec618716d24fb5e9c0d6a5cbf26d735b9
Nathan Basanese (git.io/v6) <v6@users.noreply.github.com>
2018-08-21 16:20:12 -0700

    // , Begin adding files for Vault EaaS Demo

---
+++ demonstrations/encryption/generate_key.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo '  ##  Generate an Encryption Key with Vault  ##  '
+
+echo "
+  ##	Hands-on Demo Key Generation (5-10 minutes)
+  ##	https://www.vaultproject.io/api/secret/transit/index.html#create-key
+  ##                    Vault analogy to openssl genrsa or openssl ecparam
+
+  ##  Don't forget to set up your VAULT_TOKEN and VAULT_ADDR variables, first.
+  ##  For example: 
+  ##  export VAULT_TOKEN=7999a32a-15a8-ecba-2f2c-b65c52018de8
+  ##  export VAULT_ADDR=https://vault.mysite.com:8200"
+
+KEY_NAME=$1
+
+curl \
+    --header "X-Vault-Token: ${VAULT_TOKEN}" \
+    --request POST \
+    --data '{ "type": "rsa-4096", "derived": false, "exportable": true, "allow_plaintext_backup": true }' \
+    "${VAULT_ADDR}/v1/transit/keys/igp_${KEY_NAME}"
