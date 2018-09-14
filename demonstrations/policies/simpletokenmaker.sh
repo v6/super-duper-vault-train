da57aebab47dd7a316c7b7aa7ffb0ca923d3e48d
Nathan Basanese (git.io/v6) <v6@users.noreply.github.com>
2018-09-14 15:11:16 -0700

    // , Improve Token Maker output

--- demonstrations/policies/simpletokenmaker.sh
+++ demonstrations/policies/simpletokenmaker.sh
@@ -5,4 +5,4 @@ curl -k \
     --header "X-Vault-Token: ${VAULT_TOKEN}" \
     --request POST \
     --data "{ \"policies\": [ ${POLICIES} ], \"ttl\": \"350h\", \"renewable\": true }" \
-    "${VAULT_ADDR}"/v1/auth/token/create
+    "${VAULT_ADDR}"/v1/auth/token/create | jq
