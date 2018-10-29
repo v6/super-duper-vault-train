d6a0c42b5d95532c2a63d8124f907b58ade63fd5
Nathan Basanese (git.io/v6) <v6@users.noreply.github.com>
2018-10-29 16:25:33 -0700

    // , Disable Consul ACL

--- provision_consul/scripts/acl/consul_acl.sh
+++ provision_consul/scripts/acl/consul_acl.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-export CONSUL_ADDR=http://127.0.0.1:8500
-
-echo 'ps -ef | grep consul'
-ps -ef | grep consul
-
-echo "Now attempting to 'bootstrap' Consul ACLs for the Consul cluster with an API accessible at the following address:"
-echo "${CONSUL_ADDR}"
-echo "export CONSUL_HTTP_TOKEN=$(curl -k --request PUT \"${CONSUL_ADDR}/v1/acl/bootstrap\" | jq -r '.ID')"
-curl -k --request PUT "${CONSUL_ADDR}/v1/acl/bootstrap"  ##  Test Only
-export CONSUL_HTTP_TOKEN=$(curl -k --request PUT "${CONSUL_ADDR}/v1/acl/bootstrap" | jq -r '.ID')
-echo "CONSUL_HTTP_TOKEN=${CONSUL_HTTP_TOKEN}"
-echo "${CONSUL_HTTP_TOKEN}" > "/vagrant/${HOSTNAME}_consul_http_token.txt"
+#export CONSUL_ADDR=http://127.0.0.1:8500
+#
+#echo 'ps -ef | grep consul'
+#ps -ef | grep consul
+#
+#echo "Now attempting to 'bootstrap' Consul ACLs for the Consul cluster with an API accessible at the following address:"
+#echo "${CONSUL_ADDR}"
+#echo "export CONSUL_HTTP_TOKEN=$(curl -k --request PUT \"${CONSUL_ADDR}/v1/acl/bootstrap\" | jq -r '.ID')"
+#curl -k --request PUT "${CONSUL_ADDR}/v1/acl/bootstrap"  ##  Test Only
+#export CONSUL_HTTP_TOKEN=$(curl -k --request PUT "${CONSUL_ADDR}/v1/acl/bootstrap" | jq -r '.ID')
+#echo "CONSUL_HTTP_TOKEN=${CONSUL_HTTP_TOKEN}"
+#echo "${CONSUL_HTTP_TOKEN}" > "/vagrant/${HOSTNAME}_consul_http_token.txt"
 
   ##  Should display a Consul token, and write it to a file.
   ##  That file should be deleted ASAP, and its contents saved to a secure location
