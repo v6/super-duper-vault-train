9166b81b4d0b16fa8dfa758f53a0a6cf469b06cc
Nathan Basanese (git.io/v6) <v6@users.noreply.github.com>
2018-11-07 14:12:37 -0800

    // , Add root credential rotation to demo
    Update vaultdownload.sh
    
      // , Custom file locations
    
      // ,   ##  If someone passes in a third argument, put all files other than the binary under
      ##  a specific folder.
      ##  If no one passes in a third argument, this has no effect.
    
      // , Parameterize Vault Download
    
    This fixes issues with the parameterized script

--- vaultdownload.sh
+++ vaultdownload.sh
@@ -9,6 +9,8 @@ VAULT_FOLDER_PREFIX=$3
 
   ##  Version determined by $1, e.g. 0.10.2
 curl -s -o "/tmp/vault_${1}_linux_amd64.zip" "https://releases.hashicorp.com/vault/${1}/vault_${1}_linux_amd64.zip"
+mkdir -p -v -m 0755 "${VAULT_BINARY_FOLDER}"
+echo "unzip -o /tmp/vault_${1}_linux_amd64.zip -d ${VAULT_BINARY_FOLDER}"
 unzip -o /tmp/vault_${1}_linux_amd64.zip -d "${VAULT_BINARY_FOLDER}"
 echo "Vault Release: ${1}"
 echo "Vault Download: https://releases.hashicorp.com/vault/${1}/vault_${1}_linux_amd64.zip"
@@ -21,6 +23,6 @@ chown -R vault:vault "${VAULT_FOLDER_PREFIX}/etc/vault.d" "${VAULT_FOLDER_PREFIX
 touch "${VAULT_FOLDER_PREFIX}/etc/vault.d/vault.hcl"
 chmod -R 0644 $VAULT_FOLDER_PREFIX/etc/vault.d/*
 chown vault:vault "${VAULT_BINARY_FOLDER}/vault"
-chmod 755 "${VAULT_BINARY_PATH}/vault"
+chmod 755 "${VAULT_BINARY_FOLDER}/vault"
 
 echo 'export VAULT_ADDR=http://localhost:8200  ##  Add Vault address to startup script'|sudo tee /etc/profile.d/vault.sh
