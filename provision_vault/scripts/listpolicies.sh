d0f7c951e35341286506c24ef510d3b317249c37
Hazzim Anaya <hazzim.anaya@gmail.com>
2019-04-18 11:57:39 -0700

    Update listpolicies.sh
    -curl with the method GET gives the error "unsupported operation", works fine with the method LIST. 
    -Add "'.data.keys'" after jq to display only the list of policies.

--- provision_vault/scripts/listpolicies.sh
+++ provision_vault/scripts/listpolicies.sh
@@ -2,12 +2,12 @@
 
   ##  Get global list of policies
 curl -k \
-    --request GET \
+    --request LIST \
     --header "X-Vault-Token: $VAULT_TOKEN" \
-    "$VAULT_ADDR/v1/sys/policies/acl" | jq
+    "$VAULT_ADDR/v1/sys/policies/acl" | jq '.data.keys'
 
   ##  List specific policy
 curl -k \
-    --request GET \
+    --request LIST \
     --header "X-Vault-Token: $VAULT_TOKEN" \
     "$VAULT_ADDR/v1/sys/policies/acl/admin" | jq
